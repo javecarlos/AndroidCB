80a43f57dfe2682c40cae8122ccea8f9b0d8753c
javecarlos <jave.carlos@gmail.com>
2016-05-21 16:19:55 -0500

    modificaciones en splash screen, agregando estilos para NoActionBar

--- app/src/main/java/trabajofinalcb/javecarlos/pe/trabajofinalcb/SplashScreenActivity.java
+++ app/src/main/java/trabajofinalcb/javecarlos/pe/trabajofinalcb/SplashScreenActivity.java
@@ -2,21 +2,20 @@ package trabajofinalcb.javecarlos.pe.trabajofinalcb;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import utils.Constantes;
 
 /**
  * Created by Administrator on 18/05/2016.
  */
 public class SplashScreenActivity extends AppCompatActivity {
+    private TextView tvProgress;
     private ProgressBar progressBar;
-    private static final String TAG = "SplashActivity";
+    int progressStatusCounter = 0;
+    Handler progressHandler = new Handler();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,26 +23,33 @@ public class SplashScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
 
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        progressBar.setMax(100);
+        progressBar.setScaleY(3f);
 
-        Thread timerThread = new Thread() {
+        tvProgress = (TextView) findViewById(R.id.tvProgress);
+
+        Thread timerThread = new Thread(new Runnable() {
             public void run() {
-                try{
-                    for (int i=1; i<=100;i++){
-                        sleep(50);
-                        progressBar.setProgress(i);
+                while (progressStatusCounter < 100) {
+                    progressStatusCounter += 1;
+                    progressHandler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressStatusCounter);
+                            //actualizando el estado del textview
+                            tvProgress.setText(progressStatusCounter + " %");
+                        }
+                    });
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-                }catch(InterruptedException e){
-                    Log.d(TAG, e.getMessage());
-                    Toast toastMessage = Toast.makeText(getApplicationContext(), Constantes.ERROR_NO_CONTROLADO, Toast.LENGTH_SHORT);
-                    toastMessage.show();
-                }finally{
-                    Intent intentLogin = new Intent(SplashScreenActivity.this, LoginActivity.class);
-                    startActivity(intentLogin);
-                    finish();
                 }
+
+                Intent intentLogin = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intentLogin);
+                finish();
             }
-        };
+        });
         timerThread.start();
     }
 }
